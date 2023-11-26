5985c0bbfbf462f4c97e96117c755630b8522408
Daniel Wolf <daniel-wolf@outlook.com>
2023-11-26 21:55:24 +0100

    add identity user

--- ECommerce/ECommerce.DataAccess/Data/ApplicationDbContext.cs
+++ ECommerce/ECommerce.DataAccess/Data/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ public class ApplicationDbContext : IdentityDbContext
 {
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
+    public DbSet<ApplicationUser> applicationUsers { get; set; }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {

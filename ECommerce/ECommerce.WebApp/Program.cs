2c76c40cde0045167d6997f5bd5350361b0099a2
Daniel Wolf <daniel-wolf@outlook.com>
2023-11-27 20:14:06 +0100

    update

--- ECommerce/ECommerce.WebApp/Program.cs
+++ ECommerce/ECommerce.WebApp/Program.cs
@@ -13,7 +13,10 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"), x => x.MigrationsAssembly("ECommerce.DataAccess")));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();

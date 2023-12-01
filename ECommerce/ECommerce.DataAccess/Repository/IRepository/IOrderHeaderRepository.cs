cf8dc75b36d4f700c620ac6b0902ad383279102a
Daniel Wolf <daniel-wolf@outlook.com>
2023-12-01 16:51:01 +0100

    update

--- ECommerce/ECommerce.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
+++ ECommerce/ECommerce.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
@@ -5,4 +5,6 @@ namespace ECommerce.DataAccess.Repository.IRepository;
 public interface IOrderHeaderRepository : IRepository<OrderHeader>
 {
     void Update(OrderHeader orderHeader);
+    void UpdateStatus(int id, string orderStatus, string? paymentStatus = null);
+    void UpdatePaymentId(int id, string sessionId, string paymentIntentId);
 }
--- ECommerce/ECommerce.DataAccess/Repository/OrderHeaderRepository.cs
+++ ECommerce/ECommerce.DataAccess/Repository/OrderHeaderRepository.cs
@@ -10,5 +10,33 @@ namespace ECommerce.DataAccess.Repository
         {
             _context.Update(orderHeader);
         }
+
+        public void UpdatePaymentId(int id, string sessionId, string paymentIntentId)
+        {
+            var oderFromDb = _context.OrderHeaders.SingleOrDefault(x => x.Id == id);
+
+            if (!string.IsNullOrEmpty(sessionId))
+            {
+                oderFromDb!.SessionId = sessionId;
+            }
+            if (!string.IsNullOrEmpty(paymentIntentId))
+            {
+                oderFromDb!.PaymentIntentId = paymentIntentId;
+                oderFromDb!.PaymentDate = DateTime.UtcNow;
+            }
+        }
+
+        public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+        {
+            var oderFromDb = _context.OrderHeaders.SingleOrDefault(x => x.Id == id);
+            if (oderFromDb != null)
+            {
+                oderFromDb.OrderStatus = orderStatus;
+                if (!string.IsNullOrEmpty(paymentStatus))
+                {
+                    oderFromDb.PaymentStatus = paymentStatus;
+                }
+            }
+        }
     }
 }

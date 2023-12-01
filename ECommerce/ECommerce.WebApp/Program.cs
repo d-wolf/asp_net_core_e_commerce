fad3d93546383e4e4518ea8e20e6c40b2a5849ad
Daniel Wolf <daniel-wolf@outlook.com>
2023-12-01 15:18:48 +0100

    update

--- ECommerce/ECommerce.WebApp/Program.cs
+++ ECommerce/ECommerce.WebApp/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using ECommerce.Utility;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Stripe;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -41,7 +42,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();

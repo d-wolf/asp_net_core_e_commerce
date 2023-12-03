22a7198b8e5f9c4d857f6e89624466ffbc2245cb
Daniel Wolf <daniel-wolf@outlook.com>
2023-12-03 18:01:13 +0100

    update

--- ECommerce/ECommerce.WebApp/Areas/Admin/Controllers/OrderController.cs
+++ ECommerce/ECommerce.WebApp/Areas/Admin/Controllers/OrderController.cs
@@ -1,5 +1,6 @@
 using ECommerce.DataAccess.Repository.IRepository;
 using ECommerce.Models.Models;
+using ECommerce.Models.ViewModels;
 using ECommerce.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -17,12 +18,50 @@ public class OrderController(IUnitOfWork unitOfWork) : Controller
         return View();
     }
 
+    public IActionResult Details(int? id)
+    {
+        var header = _unitOfWork.OrderHeader.GetFirstOrDefault(x => x.Id == id, includeProperties: nameof(ApplicationUser));
+        var details = _unitOfWork.OrderDetail.GetAll(x => x.OrderHeaderId == id, includeProperties: nameof(Product)) ?? Enumerable.Empty<OrderDetail>();
+
+        if (header == null)
+        {
+            return NotFound();
+        }
+
+        var orderVM = new OrderVM
+        {
+            OrderHeader = header,
+            OrderDetails = details ?? Enumerable.Empty<OrderDetail>(),
+        };
+
+        return View(orderVM);
+    }
+
     #region API Calls
 
     [HttpGet]
-    public IActionResult GetAll()
+    public IActionResult GetAll(string? status)
     {
-        var orderList = _unitOfWork.OrderHeader.GetAll(includeProperties: nameof(ApplicationUser)).ToList();
+        var orderList = _unitOfWork.OrderHeader.GetAll(includeProperties: nameof(ApplicationUser));
+
+        switch (status)
+        {
+            case "pending":
+                orderList = orderList.Where(x => x.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                break;
+            case "inprocess":
+                orderList = orderList.Where(x => x.OrderStatus == SD.StatusProcessing);
+                break;
+            case "completed":
+                orderList = orderList.Where(x => x.OrderStatus == SD.StatusShipped).ToList();
+                break;
+            case "approved":
+                orderList = orderList.Where(x => x.OrderStatus == SD.StatusApproved).ToList();
+                break;
+            default:
+                break;
+        }
+
         return Json(new { data = orderList });
     }
     #endregion
